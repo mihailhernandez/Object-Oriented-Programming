85b9197e32aef9a777f73b10f9e56a53e0bf7c33
Mihail Hernandez <mihailernandes@gmail.com>
2019-05-12 02:53:34 +0300

    Student class improvements.

--- Practices/Projects/OOP Final Project/Student booklet/Student.cpp
+++ Practices/Projects/OOP Final Project/Student booklet/Student.cpp
@@ -1,9 +1,12 @@
+
 #include "Student.h"
 #include "Common.h"
 
+#include <cstring>
+
 Student::Student() : faculty_number(0), first_name(nullptr), last_name(nullptr) {}
 
-Student::Student(const unsigned int faculty_number, const char* first_name, const char* last_name)
+Student::Student(const int faculty_number, const char* first_name, const char* last_name)
 	: faculty_number(0), first_name(nullptr), last_name(nullptr)
 {
 	set_faculty_number(faculty_number);
@@ -19,8 +22,7 @@ Student::Student(const Student& other_student)
 
 Student::~Student()
 {
-	delete[] first_name;
-	delete[] last_name;
+	free();
 }
 
 Student& Student::operator=(const Student& other_student)
@@ -46,30 +48,32 @@ void Student::set_last_name(const char* last_name)
 	this->last_name = Utils::copy_string(last_name);
 }
 
-unsigned int Student::get_faculty_number() const
+int Student::get_faculty_number() const
 {
 	return this->faculty_number;
 }
 
-char* Student::get_first_name() const
+const char* Student::get_first_name() const
 {
-	return Utils::copy_string(this->first_name);
+	return first_name;
 }
 
-char* Student::get_last_name() const
+const char* Student::get_last_name() const
 {
-	return Utils::copy_string(this->last_name);
+	return this->last_name;
 }
 
 void Student::copy(const Student& other_student)
 {
-	char* other_student_first_name = other_student.get_first_name();
-	char* other_student_last_name = other_student.get_last_name();
 	set_faculty_number(other_student.get_faculty_number());
-	set_first_name(other_student_first_name);
-	set_last_name(other_student_last_name);
-	delete[] other_student_first_name;
-	delete[] other_student_last_name;
+	set_first_name(other_student.get_first_name());
+	set_last_name(other_student.get_last_name());
+}
+
+void Student::free()
+{
+	delete[] first_name;
+	delete[] last_name;
 }
 
 std::istream& operator>>(std::istream& input_stream, Student& student)
@@ -88,21 +92,68 @@ std::istream& operator>>(std::istream& input_stream, Student& student)
 
 std::ostream& operator<<(std::ostream& output_stream, const Student& student)
 {
-	char* first_name = student.get_first_name();
-	char* last_name = student.get_last_name();
 	output_stream << student.get_faculty_number() << " "
-		<< first_name << " " << last_name;
-	delete[] first_name;
-	delete[] last_name;
+		<< student.get_first_name() << " " 
+		<< student.get_last_name();
 	return output_stream;
 }
 std::ifstream& operator>>(std::ifstream& input_file_stream, Student& student)
 {
-	//TODO: implement me!
+	int faculty_number = 0;
+	int first_name_len = 0;
+	int last_name_len = 0;
+	char* first_name = nullptr;
+	char* last_name = nullptr;
+	input_file_stream.read((char*)& faculty_number, sizeof(int));
+	input_file_stream.read((char*)& first_name_len, sizeof(int));
+	first_name = new char[first_name_len + 1];
+	input_file_stream.read((char*)& first_name, sizeof(char) * first_name_len);
+	first_name[first_name_len] = '\0';
+	input_file_stream.read((char*)& last_name_len, sizeof(int));
+	last_name = new char[last_name_len + 1];
+	input_file_stream.read((char*)& last_name, sizeof(char) * last_name_len);
+	last_name[last_name_len] = '\0';
+	student.set_faculty_number(faculty_number);
+	student.set_first_name(first_name);
+	student.set_last_name(last_name);
+	delete[] first_name;
+	delete[] last_name;
 	return input_file_stream;
 }
 std::ofstream& operator<<(std::ofstream& output_file_stream, const Student& student)
 {
-	//TODO: implement me!
+	const char* first_name = student.get_first_name();
+	const char* last_name = student.get_last_name();
+	int faculty_number = student.get_faculty_number();
+	int first_name_len = strlen(first_name);
+	int last_name_len = strlen(last_name);
+	output_file_stream.write((const char*)& faculty_number, sizeof(int));
+	output_file_stream.write((const char*)& first_name_len, sizeof(int));
+	output_file_stream.write((const char*)& first_name, sizeof(char) * first_name_len);
+	output_file_stream.write((const char*)& last_name_len, sizeof(int));
+	output_file_stream.write((const char*)& last_name, sizeof(char) * last_name_len);
 	return output_file_stream;
+}
+
+std::istream& operator>>(std::istream& input_stream, Student* student)
+{
+	input_stream >> *(student);
+	return input_stream;
+}
+
+std::ostream& operator<<(std::ostream& output_stream, const Student* student)
+{
+	output_stream << *(student);
+	return output_stream;
+}
+
+std::ifstream& operator>>(std::ifstream& file_input_stream, Student* student)
+{
+	file_input_stream >> (*student);
+	return file_input_stream;
+}
+std::ofstream& operator<<(std::ofstream& file_output_stream, const Student* student)
+{
+	file_output_stream << *(student);
+	return file_output_stream;
 }
--- Practices/Projects/OOP Final Project/Student booklet/Student.h
+++ Practices/Projects/OOP Final Project/Student booklet/Student.h
@@ -7,29 +7,35 @@ class Student
 {
 public:
 	Student();
-	Student(const unsigned int, const char*, const char*);
+	Student(const int, const char*, const char*);
 	~Student();
 
 	Student(const Student&);
 	Student& operator=(const Student&);
 	
-	unsigned int get_faculty_number() const;
-	char* get_first_name() const;
-	char* get_last_name() const;
+	int get_faculty_number() const;
+	const char* get_first_name() const;
+	const char* get_last_name() const;
 
 	void set_faculty_number(const unsigned int);
 	void set_first_name(const char*);
 	void set_last_name(const char*);
 	
 private:
-	unsigned int faculty_number;
+	int faculty_number;
 	char* first_name;
 	char* last_name;
 
 	void copy(const Student&);
+	void free();
 };
 
 std::istream& operator>>(std::istream&, Student&);
 std::ostream& operator<<(std::ostream&, const Student&);
 std::ifstream& operator>>(std::ifstream&, Student&);
 std::ofstream& operator<<(std::ofstream&, const Student&);
+
+std::istream& operator>>(std::istream&, Student*);
+std::ostream& operator<<(std::ostream&, const Student*);
+std::ifstream& operator>>(std::ifstream&, Student*);
+std::ofstream& operator<<(std::ofstream&, const Student*);
